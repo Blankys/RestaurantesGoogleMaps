9d00c8137f992c72ededb29293b2a4058e9cd1de
Blankys <flori_blank@hotmail.com>
2018-02-12 09:55:06 -0600

    Incluyendo datos de la BD

---
+++ _basedatos/INSERTS.sql
@@ -0,0 +1,14 @@
+USE RESTAURANTES;
+--
+-- Volcado de datos para la tabla `restaurante`
+--
+
+INSERT INTO `restaurante` (`IDRESTAURANTE`, `NOMRESTAURANTE`, `DESCRESTAURANTE`, `DIRECCIONRESTAURANTE`, `HORARIORESTAURANTE`, `NUMRESTAURANTE`, `LONGITUD`, `LATITUD`) VALUES
+(1, 'Love Fish', 'Best seafood restaurant', '580 Darling Street, Rozelle, NSW', '8:00AM - 11:00PM', '22072577', 151.171936, -33.861034),
+(2, 'Young Henrys', 'Fast food and the best burguers', '76 Wilford Street, Newtown, NSW', '8:00AM - 11:00PM', '22334455', 151.174469, -33.898113),
+(3, 'Hunter Gatherer', 'HG Is an casual dining scene', 'Greenwood Plaza, 36 Blue St, North Sydney NSW', '8:00AM-11:00PM', '22334456', 151.207474, -33.840282),
+(4, 'Tonys', 'Is open all day for breakfast', '7A, 2 Huntley Street, Alexandria, NSW', '8:00AM-11:00PM', '22334456', 151.194168, -33.910751),
+(5, 'Nomad', 'A quality place to eat in family', '16 Foster Street, Surry Hills, NSW', '8:00AM-11:00PM', '22334456', 151.210449, -33.879917);
+
+COMMIT;
+
